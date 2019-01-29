e7ddcb4eec37bd72c5792ab24ea9dbe83c3867ad
MatejMaricic <matej.maricic@gmail.com>
2019-01-29 10:26:39 +0100

    code refactor in Statistics

--- Statistics.php
+++ Statistics.php
@@ -34,13 +34,14 @@ class Statistics extends Users
 
                 case 3:
 
-                       $this->totalIncome(); break;
+                    $this->totalIncome();
+                    break;
 
 
                 case 4:
 
-                      $this->average_by_gender();   break;
-
+                    $this->average_by_gender();
+                    break;
 
 
             }
@@ -69,13 +70,14 @@ class Statistics extends Users
         $today = new DateTime('NOW');
         $person = $this->employeeStorage->getEmployee($id);
 
-                $employeeDate = new DateTime($person['birth']);
-                $employeeDiff = $today->diff($employeeDate);
-                $employeeAge = $employeeDiff->y;
+        $employeeDate = new DateTime($person['birth']);
+        $employeeDiff = $today->diff($employeeDate);
+        $employeeAge = $employeeDiff->y;
 
-            return $employeeAge;
+        return $employeeAge;
 
     }
+
     //finds employee gender based on id sent by another method
     public function employeeGender($id)
     {
@@ -95,31 +97,30 @@ class Statistics extends Users
         $total_under_40_income = 0;
         $total_over_40_income = 0;
 
-        foreach ($this->employeeStorage->getEmployees() as $key => $value)
-        {
+        foreach ($this->employeeStorage->getEmployees() as $key => $value) {
             $age = $this->employeeAge($key);
-            if ($age < 20){
+            if ($age < 20) {
 
                 $total_under_20_income += $value['income'];
 
-            }elseif ($age >=20 && $age <30){
+            } elseif ($age >= 20 && $age < 30) {
 
                 $total_under_30_income += $value['income'];
-            }elseif ($age >=30 && $age <40){
+            } elseif ($age >= 30 && $age < 40) {
 
                 $total_under_40_income += $value['income'];
-            }elseif ($age >40){
+            } elseif ($age > 40) {
 
                 $total_over_40_income += $value['income'];
             }
 
         }
-        echo "\033[32m". "UKUPNA PRIMANJA ZAPOSLENIKA PO DOBNIM SKUPINAMA \n\n"."\033[0m";
+        echo "\033[32m" . "UKUPNA PRIMANJA ZAPOSLENIKA PO DOBNIM SKUPINAMA \n\n" . "\033[0m";
 
-        echo "\033[32m". "## Zaposlenici mlađi od 20: $total_under_20_income kn \n\n"."\033[0m";
-        echo "\033[32m". "## Zaposlenici mlađi od 30: $total_under_30_income kn \n\n"."\033[0m";
-        echo "\033[32m". "## Zaposlenici mlađi od 40: $total_under_40_income kn \n\n"."\033[0m";
-        echo "\033[32m". "## Zaposlenici stariji od 40: $total_over_40_income kn \n\n"."\033[0m";
+        echo "\033[32m" . "## Zaposlenici mlađi od 20: $total_under_20_income kn \n\n" . "\033[0m";
+        echo "\033[32m" . "## Zaposlenici mlađi od 30: $total_under_30_income kn \n\n" . "\033[0m";
+        echo "\033[32m" . "## Zaposlenici mlađi od 40: $total_under_40_income kn \n\n" . "\033[0m";
+        echo "\033[32m" . "## Zaposlenici stariji od 40: $total_over_40_income kn \n\n" . "\033[0m";
 
     }
 
@@ -133,29 +134,28 @@ class Statistics extends Users
         $average_male = 0;
         $average_female = 0;
         $difference = 0;
-        foreach ($this->employeeStorage->getEmployees() as $key =>$value)
-        {
+        foreach ($this->employeeStorage->getEmployees() as $key => $value) {
             $gender = $this->employeeGender($key);
 
-            if ($gender === 'M'){
+            if ($gender === 'M') {
 
                 $male_income += $value['income'];
                 $m++;
-            }elseif ($gender === 'Ž' || $gender === 'Z'){
+            } elseif ($gender === 'Ž' || $gender === 'Z') {
 
                 $female_income += $value['income'];
                 $f++;
             }
 
         }
-        $average_male = $male_income/$m;
-        $average_female = $female_income/$f;
+        $average_male = $male_income / $m;
+        $average_female = $female_income / $f;
         $difference = abs($average_female - $average_male);
 
-        echo "\033[32m". "PROSJEČNA PRIMANJA \n\n"."\033[0m";
-        echo "\033[32m". "## Prosječna primanja kod žena: $average_female kn \n\n"."\033[0m";
-        echo "\033[32m". "## Prosječna primanja kod muškaraca: $average_male kn \n\n"."\033[0m";
-        echo "\033[32m". "## Razlika u prosječnim primanjima između žena i muškaraca: $difference kn \n\n"."\033[0m";
+        echo "\033[32m" . "PROSJEČNA PRIMANJA \n\n" . "\033[0m";
+        echo "\033[32m" . "## Prosječna primanja kod žena: $average_female kn \n\n" . "\033[0m";
+        echo "\033[32m" . "## Prosječna primanja kod muškaraca: $average_male kn \n\n" . "\033[0m";
+        echo "\033[32m" . "## Razlika u prosječnim primanjima između žena i muškaraca: $difference kn \n\n" . "\033[0m";
 
     }
 
@@ -165,31 +165,31 @@ class Statistics extends Users
         $today = new DateTime('NOW');
         $totalDays = 0;
 
-        foreach( $this->employeeStorage->getEmployees() as $employee ){
-            $employeeDate = new DateTime( $employee['birth'] );
+        foreach ($this->employeeStorage->getEmployees() as $employee) {
+            $employeeDate = new DateTime($employee['birth']);
             $employeeDiffDate = $today->diff($employeeDate)->format("%a");
-            $totalDays += (int) $employeeDiffDate;
+            $totalDays += (int)$employeeDiffDate;
         }
 
         $startDate = new DateTime(date("Y/m/d"));
-        $endDate = new DateTime(date("Y/m/d",strtotime("+$totalDays days")));
-        $diff = date_diff($startDate,$endDate);
+        $endDate = new DateTime(date("Y/m/d", strtotime("+$totalDays days")));
+        $diff = date_diff($startDate, $endDate);
 
 
-        echo "\033[32m". "## Ukupna starost zaposlenika je ".$diff->y." godina, ".$diff->m." mjeseci i ".$diff->d." dana \n\n"."\033[0m";
+        echo "\033[32m" . "## Ukupna starost zaposlenika je " . $diff->y . " godina, " . $diff->m . " mjeseci i " . $diff->d . " dana \n\n" . "\033[0m";
     }
 
-    public function averageAge(){
+    public function averageAge()
+    {
 
         $totalAge = 0;
-        foreach ($this->employeeStorage->getEmployees() as $key => $value)
-        {
+        foreach ($this->employeeStorage->getEmployees() as $key => $value) {
             $totalAge += $this->employeeAge($key);
         }
 
-        $averageAge = round($totalAge / count($this->employeeStorage->getEmployees()) );
+        $averageAge = round($totalAge / count($this->employeeStorage->getEmployees()));
 
-        echo "\033[32m". "## Prosječna starost zaposlenika je $averageAge godina \n\n"."\033[0m";
+        echo "\033[32m" . "## Prosječna starost zaposlenika je $averageAge godina \n\n" . "\033[0m";
     }
 
 }
